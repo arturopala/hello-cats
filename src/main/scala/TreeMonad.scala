bc96c919b9a0f914e1426c595d9840497a2e0862
Artur Opala <artur.opala@siili.fi>
2017-10-11 18:58:26 +0100

    stack safe impl of flatMap

--- src/main/scala/TreeMonad.scala
+++ src/main/scala/TreeMonad.scala
@@ -27,42 +27,51 @@ object TreeMonad {
 
     override def pure[A](x: A): Tree[A] = Leaf(x)
 
-    final override def flatMap[A, B](fa: Tree[A])(f: (A) => Tree[B]): Tree[B] = fa match {
-      case Leaf(x) => f(x)
-      case Branch(left, right) => Branch(flatMap(left)(f), flatMap(right)(f))
+    final override def flatMap[A, B](fa: Tree[A])(f: (A) => Tree[B]): Tree[B] = {
+      @tailrec
+      def m(stack: List[Tree[A]], c: Builder[B]): Tree[B] = stack match {
+        case Nil => c match {
+          case Result(Some(tree)) => tree
+          case _ => throw new IllegalStateException(s"expected Result(Some(tree)) but was $c")
+        }
+        case Leaf(x) :: xs => m(xs, c(Some(f(x))))
+        case Branch(l, r) :: xs => m(l :: r :: xs, ComposeBranch(c))
+      }
+      m(List(fa), Result())
     }
 
     final override def tailRecM[A, B](a: A)(f: (A) => Tree[Either[A, B]]): Tree[B] = {
-
-      sealed trait Compose extends Function[Option[Tree[B]], Compose]
-      case class GetResult(var tree: Option[Tree[B]] = None) extends Compose {
-        def apply(o: Option[Tree[B]]): Compose = { tree = o; this}
-      }
-      case class ComposeBranch(c: Compose) extends Compose {
-        def apply(o1: Option[Tree[B]]): Compose = o1 match {
-          case None => c
-          case Some(left) => new Compose {
-            def apply(o2: Option[Tree[B]]): Compose = o2 match {
-              case None => c(Some(left))
-              case Some(right) => c(Some(Branch(left, right)))
-            }
-          }
-        }
-      }
-
       @tailrec
-      def m(stack: List[Tree[Either[A, B]]], c: Compose): Tree[B] = stack match {
+      def m(stack: List[Tree[Either[A, B]]], c: Builder[B]): Tree[B] = stack match {
         case Nil => c match {
-          case GetResult(Some(tree)) => tree
-          case _ => throw new IllegalStateException(s"expected tree result but was $c")
+          case Result(Some(tree)) => tree
+          case _ => throw new IllegalStateException(s"expected Result(Some(tree)) but was $c")
         }
         case Leaf(Right(x)) :: xs => m(xs, c(Some(Leaf(x))))
         case Leaf(Left(y)) :: xs => m(f(y) :: xs, c)
-        case Branch(l,r) :: xs => m(l :: r :: xs, ComposeBranch(c))
+        case Branch(l, r) :: xs => m(l :: r :: xs, ComposeBranch(c))
       }
-
-      m(List(f(a)), GetResult())
+      m(List(f(a)), Result())
     }
+
+    sealed trait Builder[A] extends Function[Option[Tree[A]], Builder[A]]
+
+    case class Result[A](var tree: Option[Tree[A]] = None) extends Builder[A] {
+      def apply(o: Option[Tree[A]]): Builder[A] = { tree = o; this }
+    }
+
+    case class ComposeBranch[A](c: Builder[A]) extends Builder[A] {
+      def apply(o1: Option[Tree[A]]): Builder[A] = o1 match {
+        case None => c
+        case Some(left) => new Builder[A] {
+          def apply(o2: Option[Tree[A]]): Builder[A] = o2 match {
+            case None => c(Some(left))
+            case Some(right) => c(Some(Branch(left, right)))
+          }
+        }
+      }
+    }
+
   }
 
 }
